fbbeb4d6a5b975ed955f957e9f813c09bdc8ded1
huanbird <737558850@qq.com>
2018-03-03 09:40:40 +0000

    finish user edit update destroy action

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -7,6 +7,13 @@ use App\Http\Requests;
 use Auth;
 class SessionController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('session.create');
@@ -20,7 +27,7 @@ class SessionController extends Controller
         if (Auth::attempt($credentials,$request->has('remember'))){
             //登陆成功
             session()->flash('success','欢迎回来');
-            return redirect()->route('users.show',[Auth::user()]);
+            return redirect()->intended(route('users.show', [Auth::user()]));
         }else{
             //登录失败
             session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
